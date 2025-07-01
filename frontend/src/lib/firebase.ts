fca2c34609bc873b02cd580aeff7664b516a7f71
Akhanali <akhanali.amangeldy@gmail.com>
2025-07-01 06:12:43 +0500

    security: move Firebase config to environment variables

--- frontend/src/lib/firebase.ts
+++ frontend/src/lib/firebase.ts
@@ -4,17 +4,17 @@ import { getDatabase } from "firebase/database";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+// Your web app's Firebase configuration using environment variables
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAaFlHlUl9XYV23rxDUjO45BddUjVQ78pQ",
-  authDomain: "quiz-cult.firebaseapp.com",
-  databaseURL: "https://quiz-cult-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "quiz-cult",
-  storageBucket: "quiz-cult.appspot.com",
-  messagingSenderId: "48334494536",
-  appId: "1:48334494536:web:24e44fd77ebf51e7fd717d",
-  measurementId: "G-HY97SK4HYP"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
