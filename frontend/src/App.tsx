3e6b0587863b4d8b30859d701d1b975f52251c37
Akhanali <akhanali.amangeldy@gmail.com>
2025-07-01 14:39:11 +0500

    new deployment to test updated workflow

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,31 +1,29 @@
-import './App.css'
-import { Route } from 'react-router-dom'
-import { Routes } from 'react-router-dom'
-import HomePage from './pages/HomePage'
-import CreateRoomPage from './pages/CreateRoomPage'
-import LobbyPage from './pages/LobbyPage'
-import JoinRoomPage from './pages/JoinRoomPage'
-import QuizPage from './pages/QuizPage'
-import ResultsPage from './pages/ResultsPage'
-import HealthMonitor from './components/HealthMonitor'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import CreateRoomPage from './pages/CreateRoomPage';
+import JoinRoomPage from './pages/JoinRoomPage';
+import LobbyPage from './pages/LobbyPage';
+import QuizPage from './pages/QuizPage';
+import ResultsPage from './pages/ResultsPage';
+import HealthMonitor from './components/HealthMonitor';
 
+// Force new deployment to use updated workflow
 function App() {
-
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/create-room" element={<CreateRoomPage />} />
-        <Route path="/room/:id" element={<LobbyPage />} />
-        <Route path="/join-room" element={<JoinRoomPage />} />
-        <Route path="/quiz/:id" element={<QuizPage />} />
-        <Route path="/results/:id" element={<ResultsPage />} />
-      </Routes>
-      
-      {/* Health Monitor - only shows in development mode */}
-      <HealthMonitor />
-    </>
-  )
+    <Router>
+      <div className="min-h-screen bg-gray-100">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/create" element={<CreateRoomPage />} />
+          <Route path="/join" element={<JoinRoomPage />} />
+          <Route path="/lobby/:id" element={<LobbyPage />} />
+          <Route path="/quiz/:id" element={<QuizPage />} />
+          <Route path="/results/:roomId" element={<ResultsPage />} />
+        </Routes>
+        {import.meta.env.MODE === 'development' && <HealthMonitor />}
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
